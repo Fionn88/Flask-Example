0baa9166507173fc22d1cccabc6d6c30b4508202
Yuan-Heng,Kuo <coco8802231225@gmail.com>
2023-09-30 14:47:25 +0800

    modify env name

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('POSTGRES_URL')
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
 
 db=SQLAlchemy(app)
 
